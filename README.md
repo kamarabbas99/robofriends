320d5a3c97da2be89fe717c0476816b840f57787
Kamarabbas Saiyed <41794314+kamarabbas99@users.noreply.github.com>
2020-06-06 17:01:54 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# zero-to-mastery
-this repo is for all the files specific to this course and i will be committing regularly .
+# robofriends
+Tutorial for udemy course - React
+To run the project:
+
+1. Clone this repo
+2. Run `npm install`
+3. Run `npm start`
---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Card =() =>{
+
+	return(
+	<div>
+       <img src='https://robohash.org/test?size=200x200' alt='robo'/>
+        <div>
+         <h2>
+        	Jane doe
+         </h2>
+         <p>jane.doe@gmail.com</p>
+        </div>
+    </div>
+    );
+}
+export default Card;
